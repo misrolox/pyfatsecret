8bed77bbcf9a696f1efcf7583827095bf8baaa8d
misrolox <mitriandrew@hotmail.com>
2024-05-03 16:20:55 +0200

    Added common setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("VERSION") as version_file:
+    VERSION = version_file.read()
+
+# with open("requirements.txt") as requirements_file:
+#     REQUIREMENTS = requirements_file.read().splitlines()
+REQUIREMENTS = ['rauth', 'requests']
+
+with open("README.md") as readme_file:
+    DESCRIPTION = readme_file.read()
+
+setup(
+    name="pyfatsecret",
+    version=VERSION,
+    license="MIT",
+    author="Andrew Mitri",
+    author_email="mitriandrew@hotmail.com",
+    description="A simple python wrapper for the Fatsecret API.",
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/misrolox/pyfatsecret",
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=REQUIREMENTS
+)
