868e195307f1e5839eb4547e821bd56fc2606b6e
misrolox <mitriandrew@hotmail.com>
2024-05-04 02:04:16 +0200

    Generated rtd docs with sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -3,26 +3,31 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('../src'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'pyfatsecret'
 copyright = '2024, Andrew Mitri'
 author = 'Andrew Mitri'
-release = '0.1'
+version = release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
---
+++ docs/pyfatsecret.rst
@@ -0,0 +1,53 @@
+pyfatsecret package
+===================
+
+Submodules
+----------
+
+pyfatsecret.autogen module
+--------------------------
+
+.. automodule:: pyfatsecret.autogen
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pyfatsecret.fatsecret module
+----------------------------
+
+.. automodule:: pyfatsecret.fatsecret
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pyfatsecret.fatsecret\_base module
+----------------------------------
+
+.. automodule:: pyfatsecret.fatsecret_base
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pyfatsecret.food module
+-----------------------
+
+.. automodule:: pyfatsecret.food
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pyfatsecret.recipe module
+-------------------------
+
+.. automodule:: pyfatsecret.recipe
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: pyfatsecret
+   :members:
+   :undoc-members:
+   :show-inheritance:
